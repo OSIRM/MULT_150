a1fc03e2853464e0e14c033bdb876ef81ca954fe
OSIRM <Osiris.m12@gmail.com>
2023-10-28 15:39:59 -0700

    Scripting 10 instant variables
    My attempt at scripting

--- OMorales_MULT150/OMorales_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ OMorales_MULT150/OMorales_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
    public GameObject prefab;
+   public GameObject Lamp;
+   public Vector3 spawnValues;
 
+    void Start ()
+    {
+
+            for (int i=1; i<=10; i++)
+             {
+                Vector3 spawnposition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
+                GameObject gameObject = Instantiate<GameObject>(Lamp, new Vector3(2f,1f,5f), Quaternion.identity);
+            }
+       
+
+    }   
     // Update is called once per frame
     void Update()
     {
