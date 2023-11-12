333e8938a4428939f50d0d2ed6d962156ce0c908
OSIRM <Osiris.m12@gmail.com>
2023-11-11 21:49:09 -0800

    Hour17n18
    My attempt at animations

--- OMorales_MULT150/OMorales_Hour17and18/OMorales_Hour17and18/Assets/Scripts/AnimationControls2.cs
+++ OMorales_MULT150/OMorales_Hour17and18/OMorales_Hour17and18/Assets/Scripts/AnimationControls2.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class AnimationControls2 : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+ {
+    if(Input.GetKeyDown(KeyCode.D))
     {
-        
+       Debug.Log("D was Pressed");
+       anim.SetTrigger("Rotate");
     }
+     if(Input.GetKeyDown(KeyCode.S))
+    {
+       Debug.Log("S was Pressed");
+       anim.SetTrigger("Rotate");
+    }
+     if(Input.GetKeyDown(KeyCode.A))
+    {
+       Debug.Log("A was Pressed");
+       anim.SetTrigger("Rotate");
+    }
+     if(Input.GetKeyDown(KeyCode.W))
+    {
+       Debug.Log("W was Pressed");
+       anim.SetTrigger("Rotate");
+    }
+ }
 }
